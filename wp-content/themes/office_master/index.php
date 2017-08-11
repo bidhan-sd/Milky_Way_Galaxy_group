f6bd03bb2db3912e6a5eb840573db3cf7457d4e4
Bidhan sutradhar <bidhanvk@gmail.com>
2017-08-11 22:49:55 +0600

    here is 3rd and 4th class code

---
+++ wp-content/themes/office_master/index.php
@@ -0,0 +1,32 @@
+<?php
+// 3rd tutorial.
+	$x = 10;
+	$_num = 10;
+	//echo "This is x value ".$x; 
+	//echo "This is x value $x "; 
+	//echo 'This is x value $x '; 
+	//echo "This is x value $_num "; 
+	
+	//Array.
+	//$var = array('One','Two','Three');
+	//echo $var[2];
+//4th tutorial
+/*	$a = 2 + 5;
+	$b = 2 - 5;
+	$c = 2 * 5;
+	$d = 2 / 5;
+	$e = 2 % 5;
+	echo $a."<br/>";
+	echo $b."<br/>";
+	echo $c."<br/>";
+	echo $d."<br/>";
+	echo $e."<br/>";
+*/
+//Assinment operator
+	/*$ab = 5;
+	$ab %= 10;
+	echo $ab;*/
+//Compare operator
+	echo "<br/>";
+	echo 6 >= '5';
+?>
