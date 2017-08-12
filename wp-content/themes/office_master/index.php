874c6a7eae3092fcc10e046eb03f0eb10de72ce0
Bidhan sutradhar <bidhanvk@gmail.com>
2017-08-12 13:19:51 +0600

    5 and 6 tutorial done here

--- wp-content/themes/office_master/index.php
+++ wp-content/themes/office_master/index.php
@@ -1,5 +1,5 @@
 <?php
-// 3rd tutorial.
+	// 3rd tutorial.
 	$x = 10;
 	$_num = 10;
 	//echo "This is x value ".$x; 
@@ -10,23 +10,103 @@
 	//Array.
 	//$var = array('One','Two','Three');
 	//echo $var[2];
-//4th tutorial
-/*	$a = 2 + 5;
-	$b = 2 - 5;
-	$c = 2 * 5;
-	$d = 2 / 5;
-	$e = 2 % 5;
-	echo $a."<br/>";
-	echo $b."<br/>";
-	echo $c."<br/>";
-	echo $d."<br/>";
-	echo $e."<br/>";
-*/
-//Assinment operator
-	/*$ab = 5;
-	$ab %= 10;
-	echo $ab;*/
-//Compare operator
-	echo "<br/>";
-	echo 6 >= '5';
+	
+	//4th tutorial
+	/*	$a = 2 + 5;
+		$b = 2 - 5;
+		$c = 2 * 5;
+		$d = 2 / 5;
+		$e = 2 % 5;
+		echo $a."<br/>";
+		echo $b."<br/>";
+		echo $c."<br/>";
+		echo $d."<br/>";
+		echo $e."<br/>";
+	*/
+	//Assinment operator
+		/*$ab = 5;
+		$ab %= 10;
+		echo $ab;*/
+	//Compare operator
+		//echo "<br/>";
+		//echo 6 >= '5';
+	
+	//5th turorial.compare operator.
+		/*echo 6 !== '6';
+		echo 7 >= 7;*/
+		
+	//Increment operator.
+		/*
+			$var = 10;
+			echo $var++;//post increment
+			echo ++$var;//pre increment
+		*/
+	//IF Statement.
+		/*
+			$tk = 10;
+			if($tk >= 50){
+				echo "Buy Two ice-cream";
+			}elseif($tk >= 10){
+				echo "Bye One ice-cream";
+			}else{
+				echo "Low Balance";
+			}
+		*/
+		
+	//6th turorial . Switch statement.
+		/*
+		$day = "fri";
+		switch($day){
+			case "fri":
+			echo "Friday";
+			break;
+			
+			case "sat":
+			echo "Saterday";
+			break;
+			
+			case "sun":
+			echo "Sunday";
+			break;
+			
+			case "mon":
+			echo "Monday";
+			break;
+			
+			case "tue":
+			echo "Tushday";
+			break;
+			
+			case "Wed":
+			echo "Wednesday";
+			break;
+			
+			default:
+			echo "thusday";
+			break;
+		}
+		*/
+		//While loop
+		$var = 0;
+		while($var <= 100){
+			echo $var . " ";
+			$var++;
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 ?>
