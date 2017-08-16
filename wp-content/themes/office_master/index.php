288b9cfde5bcfcc249d054b72a2552616de8ec16
Bidhan sutradhar <bidhanvk@gmail.com>
2017-08-16 22:39:38 +0600

    13 turorial done here

--- wp-content/themes/office_master/index.php
+++ wp-content/themes/office_master/index.php
@@ -1,131 +1,31 @@
-<?php
-	// 3rd tutorial.
-	$x = 10;
-	$_num = 10;
-	//echo "This is x value ".$x; 
-	//echo "This is x value $x "; 
-	//echo 'This is x value $x '; 
-	//echo "This is x value $_num "; 
-	
-	//Array.
-	//$var = array('One','Two','Three');
-	//echo $var[2];
-	
-	//4th tutorial
-	/*	$a = 2 + 5;
-		$b = 2 - 5;
-		$c = 2 * 5;
-		$d = 2 / 5;
-		$e = 2 % 5;
-		echo $a."<br/>";
-		echo $b."<br/>";
-		echo $c."<br/>";
-		echo $d."<br/>";
-		echo $e."<br/>";
-	*/
-	//Assinment operator
-		/*$ab = 5;
-		$ab %= 10;
-		echo $ab;*/
-	//Compare operator
-		//echo "<br/>";
-		//echo 6 >= '5';
-	
-	//5th turorial.compare operator.
-		/*echo 6 !== '6';
-		echo 7 >= 7;*/
-		
-	//Increment operator.
-		/*
-			$var = 10;
-			echo $var++;//post increment
-			echo ++$var;//pre increment
-		*/
-	//IF Statement.
-		/*
-			$tk = 10;
-			if($tk >= 50){
-				echo "Buy Two ice-cream";
-			}elseif($tk >= 10){
-				echo "Bye One ice-cream";
-			}else{
-				echo "Low Balance";
-			}
-		*/
-		
-	//6th turorial . Switch statement.
-		/*
-		$day = "fri";
-		switch($day){
-			case "fri":
-			echo "Friday";
-			break;
-			
-			case "sat":
-			echo "Saterday";
-			break;
-			
-			case "sun":
-			echo "Sunday";
-			break;
-			
-			case "mon":
-			echo "Monday";
-			break;
-			
-			case "tue":
-			echo "Tushday";
-			break;
-			
-			case "Wed":
-			echo "Wednesday";
-			break;
-			
-			default:
-			echo "thusday";
-			break;
-		}
-		*/
-		//While loop
-		/*$var = 0;
-		while($var <= 100){
-			echo $var . " ";
-			$var++;
-		}*/
-	
-	//7th turorial For loop.
-	/*$var = 101;
-	do{
-		echo $var; 
-	}while($var <= 100)*/
-	
-	/* for($var =0; $var<=100 ; $var++){
-		echo $var . " ";
-	} */
-	$arr = ['bidhan','sutra','dhar','kanchan','pur','high'];
-	foreach($arr as $value){
-		//echo $value . "<br/>";
-	}
-	
-	//This process another way.
-	$arr = ['bidhan','sutra','dhar','kanchan','pur','high'];
-	$arrlength = count($arr);
-	for($i=0;$i<$arrlength; $i++){
-		echo $i." = ".$arr[$i]."<br/>";
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title><?php bloginfo('name');?></title>
+</head>
+<body>
+	<header> 
+		<h1><a href="<?php echo site_url(); ?>"><?php bloginfo('name');?></a></h1>
+	</header>
+	<section>
+		<div class="post_area">
+			<?php
+				if(have_posts()){
+					while(have_posts()){
+						the_post();
+					?>
+						<div class="single_post">
+							<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+							<p><?php the_content(); ?></p>
+							<hr/>
+						</div>
+						
+				<?php } }else{
+					echo "No Post here";
+				}
+			?>
+		</div>
+	</section>
+</body>
+</html>
